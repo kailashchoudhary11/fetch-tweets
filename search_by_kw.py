cc1e36248d45b97ef02bee14a88132e759119f10
kai <kailashjat604@gmail.com>
2022-11-12 09:00:22 +0530

    Add functions

---
+++ search_by_kw.py
@@ -0,0 +1,18 @@
+import tweepy
+import config
+
+client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
+
+keyword = input("Enter keyword to search")
+
+query = f'{keyword} -is:retweet'
+
+response = client.search_recent_tweets(query=query, max_results=26, tweet_fields=["created_at", "lang", "public_metrics"])
+
+tweets = []
+for i, tweet in enumerate(response.data):
+    if i == 25: 
+        print("There are more than 25 tweets")
+        break
+    tweets.append(tweet.text)
+
